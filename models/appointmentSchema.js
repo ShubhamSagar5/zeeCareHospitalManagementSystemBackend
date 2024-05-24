1f054d6f30dce6df094635d4637e5612f3dc4b50
shubham <shubhamsagar628@gmail.com>
2024-05-24 19:03:48 +0530

    Done

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -24,11 +24,11 @@ const appointmentSchema = new mongoose.Schema({
     minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
     maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
   },
-  nic: {
+  aadharNumber: {
     type: String,
-    required: [true, "NIC Is Required!"],
-    minLength: [13, "NIC Must Contain Only 13 Digits!"],
-    maxLength: [13, "NIC Must Contain Only 13 Digits!"],
+    required: [true, "aadharNumber Is Required!"],
+    minLength: [13, "aadharNumber Must Contain Only 13 Digits!"],
+    maxLength: [13, "aadharNumber Must Contain Only 13 Digits!"],
   },
   dob: {
     type: Date,
